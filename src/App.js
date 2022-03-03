66af52e3f0926950f2adbe2589da550ef94f9511
vector18xd <victxd18pepe@gmail.com>
2022-03-02 22:36:54 -0500

    Button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Home } from "./components/pages/home/index";
 import { Contact} from "./components/pages/contact/contact.jsx";
+import { About } from "./components/pages/about/about";
+import { Barber } from "./components/pages/barber/barber"
 import './App.css';
 import {Routes, BrowserRouter, Route} from "react-router-dom"; 
 
@@ -10,6 +12,8 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/Contact" element={<Contact/>}/>
+                <Route path="/Abour" element={<About/>}/>
+                <Route path="/Barber" element={<Barber/>}/>
             </Routes>
         </BrowserRouter>
     </div>
--- src/components/layouts/header.jsx
+++ src/components/layouts/header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Button } from "../IU/buttons"
 import { NavLink, useLocation } from "react-router-dom";
 import { Switch } from "../IU/Switch";
@@ -6,9 +6,10 @@ import { Switch } from "../IU/Switch";
  export const Header = () =>{
     const location = useLocation();
     console.log(location.pathname);
-    if(location.pathname = '/'){
-        document.getElementById('0').style.background = "red";
-    }else if(location.pathname = '/Contact'){
+    useEffect(()=>{
+        color();
+    })
+    const color = () =>{
         document.getElementById('0').style.background = "green";
     }
      return(
@@ -16,11 +17,14 @@ import { Switch } from "../IU/Switch";
             <nav className="NavLink">
                 <NavLink to="/"><Button name="Home" text="Home"/></NavLink>
                 <NavLink to="/Contact"><Button name="Contact" text="Contact"/></NavLink>
-                {/* <NavLink><Button name =  "About" text="About"/></NavLink>  */}
+                <NavLink to="/About"><Button name="About" text="About"/></NavLink>  
+                <NavLink to="/About"><Button name="Baber Server" text="Barber Server"/></NavLink>  
             </nav>
             <nav className="Switch">
                 <Switch name="OnOff"  number='0'/>
                 <Switch name="OnOff"  number='1'/>
+                <Switch name="OnOff"  number='2'/>
+                <Switch name="OnOff"  number='3'/>
             </nav>
         </header>
      )

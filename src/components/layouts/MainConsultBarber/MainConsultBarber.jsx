e85e6f9141fa05c816321a2bbe602a5609e2bcef
SANT3R <santer28gamer@gmail.com>
2022-04-01 20:22:08 -0500

    Consultar barberos parcialmente terminado

--- src/components/layouts/MainConsultBarber/MainConsultBarber.jsx
+++ src/components/layouts/MainConsultBarber/MainConsultBarber.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Button } from '../../IU/buttons'
+import imgVanessa from '../../img/Vanessa.jpg'
+import imgGustavo from '../../img/Gustavo.jpg'
+import imgPedro from '../../img/Pedro.jpg'
 
 export const MainConsultBarber = () => {
   return (
@@ -7,27 +10,33 @@ export const MainConsultBarber = () => {
         <div id="cuadroCard">
             <section className="sec-busqueda">
                 <div id="divBusqueda">
-                    <input></input>
+                    <input placeholder="Buscar Barberos" id="input-ConsultBarber"></input>
                 </div>
             </section>
             <section className="sec-cardBarberos">
                 <article className="cardBarberos">
-                    <img src="" alt="imagen de fondo de barberos" className="img-Barbero" />
-                    <h2>Vannesa Perez</h2>
-                    <h3>ESTILISTA</h3>
-                    <Button text="PERFIL" name="btn-barbero"/>
+                    <img src={imgVanessa} alt="imagen de fondo de barberos" className="img-Barbero" />
+                    <div className="datos">
+                        <h2>Vannesa Perez</h2>
+                        <h3>ESTILISTA</h3>
+                        <Button text="PERFIL" name="btn-barbero"/>
+                    </div>
                 </article>
                 <article className="cardBarberos">
-                    <img src="" alt="imagen de fondo de barberos" className="img-Barbero" />
-                    <h2>Vannesa Perez</h2>
-                    <h3>ESTILISTA</h3>
-                    <Button text="PERFIL" name="btn-barbero"/>
+                    <img src={imgGustavo} alt="imagen de fondo de barberos" className="img-Barbero" />
+                    <div className="datos">
+                        <h2>Gustavo</h2>
+                        <h3>BARBERO</h3>
+                        <Button text="PERFIL" name="btn-barbero"/>
+                    </div>
                 </article>
                 <article className="cardBarberos">
-                    <img src="" alt="imagen de fondo de barberos" className="img-Barbero" />
-                    <h2>Vannesa Perez</h2>
-                    <h3>ESTILISTA</h3>
-                    <Button text="PERFIL" name="btn-barbero"/>
+                    <img src={imgPedro} alt="imagen de fondo de barberos" className="img-Barbero" />
+                    <div className="datos">
+                        <h2>Vannesa Perez</h2>
+                        <h3>ESTILISTA</h3>
+                        <Button text="PERFIL" name="btn-barbero"/>
+                    </div>
                 </article>
             </section>
         </div>
--- src/components/pages/ConsultBarber/ConsultBarber.jsx
+++ src/components/pages/ConsultBarber/ConsultBarber.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Header } from '../../layouts/header'
 import { Footer } from '../../layouts/footer'
 import { MainConsultBarber } from '../../layouts/MainConsultBarber/MainConsultBarber'
-import '../../estilos.css'
+import './estilosBarber.css'
 
 export const ConsultBarber = () => {
   return (

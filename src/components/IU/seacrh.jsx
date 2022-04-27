6f9361fc3f492edae50e9157358e92888f27f73b
juanmiguelosorioarroyave <jmosorio1912@gmail.com>
2022-04-27 11:53:17 -0500

    perfilfive

--- src/components/IU/seacrh.jsx
+++ src/components/IU/seacrh.jsx
@@ -3,7 +3,7 @@ export const Search = ({firstClass, placeText, btnText}) =>{
     return(
         <section className={firstClass}>
             <form action="" method="">		    
-                <input type="search" placeholder={placeText}/>		    	
+                <input type="search" placeholder={placeText}/>		   	
                 <button>{btnText}</button>
             </form>
         </section>

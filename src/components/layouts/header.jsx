4442441c4f99cd17fc3cc077e60ca4741693fae0
juanmiguelosorioarroyave <jmosorio1912@gmail.com>
2022-04-28 11:54:23 -0500

    despliegueM

--- src/components/layouts/header.jsx
+++ src/components/layouts/header.jsx
@@ -14,7 +14,15 @@ import logo from "../../../src/components/img/logo/logo-LetrasBlancas.png"
                 <NavLink to="/About"><a className="link">SOBRE NOSOTROS</a></NavLink>
                 <NavLink to="/ConsultBarber"><a className="link">BARBEROS</a></NavLink>
                 <NavLink to="/Contact"><a className="link">CONTACTENOS</a></NavLink>
-                <NavLink className="imgP" to="/Perfil"><img className="linkPerfil" src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin"/></NavLink>
+                <li>
+                    <NavLink className="imgP" to="/Perfil"><img className="linkPerfil" src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin"/></NavLink>
+                    <ul>
+                            <li><a href="#">Submenu 1</a></li>
+                            <li><a href="#">Submenu 2</a></li>
+                            <li><a href="#">Submenu 3</a></li>
+                            <li><a href="#">Submenu 4</a></li>
+                    </ul>
+                </li>
             </nav>
         </header>
      )

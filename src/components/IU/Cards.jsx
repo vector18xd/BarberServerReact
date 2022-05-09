66d7f6b783a0ca8f54141f3bcfd486f8d50ff5fc
sant <santer28gamer@gmail.com>
2022-05-09 11:47:35 -0500

    agregando informacion en modal

--- src/components/IU/Cards.jsx
+++ src/components/IU/Cards.jsx
@@ -21,8 +21,22 @@ export const Cards = () => {
             <h4 className='info'><b>Estado:</b> Activo</h4>
         </div>
         <Modal estado={estadoModal} cambiarEstado={cambiarEstadoModal} titulo="Perfil">
-          
-          <button onClick={() => cambiarEstadoModal(!estadoModal)} className="btn-cita">Pedir Cita</button>
+            <div className="informacion-modal">
+              <figure className="campoimg-modal">
+                <img src={imgGustavo} alt="El barbero Gustavo" className="img-vtn"/>
+              </figure>
+              <div className="infoBasica-modal">
+                <h3 id="nombre-modal">Gustavo Quintana</h3>
+                <h4 className='info-modal'><b>Barrio:</b> Limonar</h4>
+                <h4 className='info-modal'><b>Horarios:</b> 06:00 a 11:30 am y 02:00 a 8:00 pm</h4>
+                <h4 className='info-modal'><b>Estado:</b> Activo</h4>
+                <p>Descripción</p>
+              </div>
+            </div>
+            <div className="detalles">
+
+            </div>
+            <button onClick={() => cambiarEstadoModal(!estadoModal)} className="btn-cita">Pedir Cita</button>
         </Modal>
     </article>
   )

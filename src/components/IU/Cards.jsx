6c1b6fffacb84d44801dd21630b02a3bd2782bf3
sant <santer28gamer@gmail.com>
2022-05-12 11:52:20 -0500

    vtn modal a medias

--- src/components/IU/Cards.jsx
+++ src/components/IU/Cards.jsx
@@ -3,7 +3,7 @@ import imgGustavo from '../img/Gustavo.jpg'
 import './css/card.css'
 import "./css/modal.css"
 import { Modal } from './Modal'
-import imgLocal from "../img/local.jpg"
+// import imgLocal from "../img/local.jpg"
 
 export const Cards = () => {
   const [estadoModal, cambiarEstadoModal]= useState(true);
@@ -22,31 +22,7 @@ export const Cards = () => {
             <h4 className='info'><b>Horarios:</b> 06:00 a 11:30 am y 02:00 a 8:00 pm</h4>
             <h4 className='info'><b>Estado:</b> Activo</h4>
         </div>
-        <Modal estado={estadoModal} cambiarEstado={cambiarEstadoModal} titulo="Perfil">
-            <div className="informacion-modal">
-              <figure className="campoimg-modal">
-                <img src={imgGustavo} alt="El barbero Gustavo" className="img-vtn"/>
-              </figure>
-              <div className="infoBasica-modal">
-                <h3 id="nombre-modal">Gustavo Quintana</h3>
-                <h4 className='info-modal'><b>Especialización:</b> Barbero, peluquero</h4>
-                <h4 className='info-modal'><b>Dirección:</b> B/Limonar 3era etapa manzana 6 casa #3</h4>
-                <h4 className='info-modal'><b>Horarios:</b> 06:00 a 11:30 am y 02:00 a 8:00 pm</h4>
-                <h4 className='info-modal'><b>Estado:</b> Activo</h4>
-                <p>Descripción</p>
-              </div>
-              <div className="calendario">
-                
-              </div>
-            </div>
-            {/* <div className="detalles">
-              <h4 className='info-modal'><b>Dirección:</b> B/Limonar 3era etapa manzana 6 casa #3</h4>
-              <h4 className='info-modal'><b>Horarios:</b> 06:00 a 11:30 am y 02:00 a 8:00 pm</h4>
-              <h4 className='info-modal'><b>Estado:</b> Activo</h4>
-              <img src={imgLocal} alt="local" className="imgLocal"/>
-            </div> */}
-            <button onClick={() => cambiarEstadoModal(!estadoModal)} className="btn-cita">Pedir Cita</button>
-        </Modal>
+        <Modal estado={estadoModal} cambiarEstado={cambiarEstadoModal} titulo="Perfil"></Modal>
     </article>
   )
 }
--- src/components/pages/agendar/agendar.jsx
+++ src/components/pages/agendar/agendar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Header } from "../../layouts/header";
 import { Footer } from "../../layouts/footer";
+import { Agendamientos } from "../../layouts/agendamientos"
 export const Agendar = () =>{
   return(
     <div className="Agendar">

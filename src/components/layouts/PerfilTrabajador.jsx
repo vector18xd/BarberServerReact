656af9498c92e86ba23ff5a373f1fabda07e9e04
Mauro <mauro@MacBook-Air-de-Mauro.local>
2022-05-30 18:51:58 -0500

    boostrap3

--- src/components/layouts/PerfilTrabajador.jsx
+++ src/components/layouts/PerfilTrabajador.jsx
@@ -144,10 +144,10 @@ export const PerfilTrabajador = () => {
                     Edit
                   </a> */}
                   <div className="col">
-                    <div className="form-check form-switch">
-                      <input type="checkbox" className="form-check-input" value="" id="check1" />
-                      <label htmlFor="check1" class="form-check-label">Acepta los terminos</label>
-                    </div>
+                  <div class="form-check form-switch">
+      <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked=""/>
+      <label class="form-check-label" for="flexSwitchCheckChecked">Checked switch checkbox input</label>
+    </div>
                   </div>
                 </div>
               </div>
